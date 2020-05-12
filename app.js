f533fb6fbccf82d3757020b1f6195fdbbed47d81
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-12 15:20:51 -0400

    created a get weather function to retrive data from api.

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ $(document).ready(function () {
 
     var Cities = []
 
+    function getWeather(){
+        var city =Cities[0]
+        var QueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=63e2ab79c455ba5a3ee05762c641f525'
+        
+        $.ajax({
+            url: QueryUrl,
+            method: "GET"
+        }).then(function(response){
+            console.log(response)
+        })
+    }
+
+
+
+// event to caputure city search
     $("#search").on("click", function(event) {
         event.preventDefault();
         var city = $('#searchcity').val().trim();
         Cities.push(city)
-        console.log(Cities)
-        alert(city)
+        
+        getWeather();
+
     })
 //   wrapper closer
 });
