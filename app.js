db87cf67cd9ee769a0c63437cfe19bb5dd612b3f
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-12 16:04:01 -0400

    searches save and generate past search buttons.

--- app.js
+++ app.js
@@ -4,28 +4,38 @@ $(document).ready(function () {
 
     var Cities = []
 
-    function getWeather(){
-        var city =Cities[0]
-        var QueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=63e2ab79c455ba5a3ee05762c641f525'
-        
+    function getWeather() {
+        var city = Cities[0]
+        var QueryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=63e2ab79c455ba5a3ee05762c641f525'
+
         $.ajax({
             url: QueryUrl,
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response)
         })
     }
 
 
+    function renderSearch() {
 
-// event to caputure city search
-    $("#search").on("click", function(event) {
-        event.preventDefault();
-        var city = $('#searchcity').val().trim();
-        Cities.push(city)
-        
-        getWeather();
+        $('#search_display').empty();
 
-    })
-//   wrapper closer
-});
+        for (var i =Cities.length; i > 0; i--) {
+            
+            $("<button>").text(Cities[i]).addClass('btn text-muted').appendTo('#search_display')
+            $('<br>').appendTo("#search_display")
+        }
+    }
+        // event to caputure city search
+        $("#search").on("click", function (event) {
+            event.preventDefault();
+            var city = $('#searchcity').val().trim();
+            Cities.push(city);
+
+            getWeather();
+            renderSearch();
+
+        })
+        //   wrapper closer
+    });
